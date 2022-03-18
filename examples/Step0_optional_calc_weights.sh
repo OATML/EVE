cc30589692d31af2e823842c62f964e9e0cf4ed6
Lood <loodvn@gmail.com>
2022-03-18 11:18:02 -0400

    changed logging dir

--- examples/Step0_optional_calc_weights.sh
+++ examples/Step0_optional_calc_weights.sh
@@ -12,7 +12,7 @@
 #SBATCH --job-name="tmp_eve_weights"
 # Job array-specific
 # Nice tip: using %3a to pad job array number to 3 digits (23 -> 023)
-#SBATCH --output=slurm_files/slurm-lvn-%A_%3a-%x.out  # File to which STDOUT + STDERR will be written, %A: jobID, %a: array task ID, %x: jobname
+#SBATCH --output=logs/slurm_files/slurm-lvn-%A_%3a-%x.out  # File to which STDOUT + STDERR will be written, %A: jobID, %a: array task ID, %x: jobname
 #SBATCH --array=0-5%10  		  # Job arrays (e.g. 1-100 with a maximum of 5 jobs at once)
 ##SBATCH --array=0			      # Resubmitting / testing only first job
 
