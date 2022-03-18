59937362376eab317aaba8b4104a788fbb638c26
Lood <loodvn@gmail.com>
2022-03-18 12:07:43 -0400

    using new MSAs

--- examples/Step0_optional_calc_weights.sh
+++ examples/Step0_optional_calc_weights.sh
@@ -28,8 +28,8 @@ export CONDA_BIN=/home/lov701/miniconda3/bin/
 echo "Assuming the conda environment protein_env is up to date"
 source "$CONDA_BIN"/activate protein_env
 
-export MSA_data_folder='./data/MSA'
-export MSA_list='./data/mappings/example_mapping.csv'
+export MSA_data_folder="/n/groups/marks/users/lood/DeepSequence_runs/weights_msa_tkmer_20220227/"
+export MSA_list='./data/mappings/mapping_msa_tkmer_20220227.csv'
 export MSA_weights_location='./data/weights_check/'
 export protein_index=$SLURM_ARRAY_TASK_ID
 
