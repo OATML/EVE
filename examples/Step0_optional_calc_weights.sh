b5af044458b482e92894e577fb93a465d0f8dc84
Lood <loodvn@gmail.com>
2022-03-18 11:22:55 -0400

    removed old training flags

--- examples/Step0_optional_calc_weights.sh
+++ examples/Step0_optional_calc_weights.sh
@@ -23,18 +23,10 @@ module load gcc/6.2.0
 export MSA_data_folder='./data/MSA'
 export MSA_list='./data/mappings/example_mapping.csv'
 export MSA_weights_location='./data/weights_check/'
-export VAE_checkpoint_location='./results/VAE_parameters'
-export model_name_suffix='Jan1_PTEN_example'
-export model_parameters_location='./EVE/default_model_params.json'
-export training_logs_location='./logs/'
 export protein_index=$SLURM_ARRAY_TASK_ID
 
 python calc_weights.py \
     --MSA_data_folder ${MSA_data_folder} \
     --MSA_list ${MSA_list} \
     --protein_index ${protein_index} \
-    --MSA_weights_location ${MSA_weights_location} \
-    --VAE_checkpoint_location ${VAE_checkpoint_location} \
-    --model_name_suffix ${model_name_suffix} \
-    --model_parameters_location ${model_parameters_location} \
-    --training_logs_location ${training_logs_location}
+    --MSA_weights_location ${MSA_weights_location}
