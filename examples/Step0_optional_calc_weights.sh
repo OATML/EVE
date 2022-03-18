87c30d5f449fd7c6b25c4658eda70b26fd67f5a8
Lood <loodvn@gmail.com>
2022-03-18 11:35:01 -0400

    some slurm script changes

--- examples/Step0_optional_calc_weights.sh
+++ examples/Step0_optional_calc_weights.sh
@@ -12,20 +12,28 @@
 #SBATCH --job-name="tmp_eve_weights"
 # Job array-specific
 # Nice tip: using %3a to pad job array number to 3 digits (23 -> 023)
-#SBATCH --output=logs/slurm_files/slurm-lvn-%A_%3a-%x.out  # File to which STDOUT + STDERR will be written, %A: jobID, %a: array task ID, %x: jobname
+##SBATCH --output=logs/slurm_files/slurm-lvn-%A_%3a-%x.out  # File to which STDOUT + STDERR will be written, %A: jobID, %a: array task ID, %x: jobname
 #SBATCH --array=0-5%10  		  # Job arrays (e.g. 1-100 with a maximum of 5 jobs at once)
-##SBATCH --array=0			      # Resubmitting / testing only first job
+#SBATCH --array=0			      # Resubmitting / testing only first job
 
-hostname
-pwd
+set -e # fail fully on first line failure (from Joost slurm_for_ml)
+
+echo "hostname: $(hostname)"
+echo "Running from: $(pwd)"
 module load gcc/6.2.0
+
+export CONDA_BIN=/home/lov701/miniconda3/bin/conda
+
+# Assumes that the conda environment is up to date
+echo "Assuming the conda environment protein_env is up to date"
+source "$CONDA_BIN"/activate protein_env
 
 export MSA_data_folder='./data/MSA'
 export MSA_list='./data/mappings/example_mapping.csv'
 export MSA_weights_location='./data/weights_check/'
 export protein_index=$SLURM_ARRAY_TASK_ID
 
-python calc_weights.py \
+srun python calc_weights.py \
     --MSA_data_folder ${MSA_data_folder} \
     --MSA_list ${MSA_list} \
     --protein_index ${protein_index} \
